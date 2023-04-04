f212a4f919ee1e048d323945bf02c534cc330165
Abdullokh <118339597+Erkinovdev@users.noreply.github.com>
2023-04-04 12:40:34 +0500

    Add files via upload

---
+++ login.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<style>
+    body {
+        background-color:rgb(45, 7, 105) ;
+        background: linear-gradient(135deg, #044f48, #2a7561);
+  background: -webkit-linear-gradient(315deg, #044f48, #2a7561);
+
+    }
+</style>
+
+<body>
+<div class="conteiner">
+
+</div>
+</body>
